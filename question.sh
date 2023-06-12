d554c627d10936cf989452bac77514008bdc3119
Andrés Vanegas <112149548+Avanegasf93@users.noreply.github.com>
2023-06-12 02:45:21 -0500

    Commit6.5

--- question.sh
+++ question.sh
@@ -44,10 +44,10 @@
 # Se utiliza el comando sed para realizar las transformaciones en los datos del archivo data.csv
 cat data.csv \
 # Agrega un cero inicial a los días y meses de una sola cifra al inicio de la línea
-sed 's/^([0-9])\/([0-9])$/0\1\/0\2/' \
+sed 's/\([0-9])\/([0-9])$/0\1\/0\2/' \
 
 # Agrega '20' al año en formato YY y cambia el orden de la fecha a DD-MM-YYYY
-sed 's/\/([0-9][0-9]);/\/20\1;/; s|([0-9]*)/([0-9]*)/([0-9]*);|\3\/\2\/\1;|' \
+sed 's/\/\([0-9][0-9]\);/\/20\1;/; s|\([0-9]*\)\/\([0-9]*\)\/\([0-9]*\);|\3\/\2\/\1;|' \
 
 # Agrega un cero inicial a los días y meses de una sola cifra en medio de la línea
 sed 's/\/([0-9])\//\/0\1\//' \
